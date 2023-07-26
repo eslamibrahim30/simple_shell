d6156d266245c6502704f5dfa5a07d9ab7bad774
hazemghaly <hazemghaly30@gmail.com>
2023-07-26 07:21:06 -0700

    shell.c file

--- shell.c
+++ shell.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "main.h"
 
 /**
  * main - Super Simple Shell.
@@ -12,36 +7,40 @@
  */
 int main(void)
 {
-	char *line = NULL;
-	char *command[] = {line, NULL};
-	pid_t child_pid;
+	char ex_ptr[5] = "exit", env_str[4] = "env";
+	char *command[BUFFER_MAX], *list_cmds[BUFFER_MAX];
+	char *line = NULL, *cmd = NULL;
+	unsigned int i = 0, j = 0;
 	size_t len = 0;
-	ssize_t nread;
+	ssize_t nread = 0;
 
 	while (1)
 	{
-		printf("#cisfun$ ");
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
+		if (isatty(fileno(stdin)))
+			printf("($) ");
+		nread = _getline(&line, &len, stdin);
+		if (nread == EOF)
 			break;
-		command[0] = strtok(line, "\n");
-		child_pid = fork();
-		if (child_pid == -1)
+		cmd = _strtok(line, "#");
+		line = cmd;
+		cmd = _strtok(line, "\n");
+		list_commands(list_cmds, cmd);
+		for (j = 0; list_cmds[j] != NULL; j++)
 		{
-			perror("./shell");
-			return (1);
-		}
-		if (child_pid == 0)
-		{
-			if (execve(command[0], command, NULL) == -1)
-			{
+			i = 0;
+			command[i] = _strtok(list_cmds[j], " ");
+			while (command[i] != NULL && i < BUFFER_MAX - 1)
+				command[++i] = _strtok(NULL, " ");
+			if (_strcmp(ex_ptr, command[0]) == 0 && command[1] != NULL)
+				_quit(_atoi(command[1]));
+			else if (_strcmp(ex_ptr, command[0]) == 0 && command[1] == NULL)
+				_quit(0);
+			else if (_strcmp(env_str, line) == 0)
+				_env();
+			else if (is_accessable(&command[0]))
+				handle_cmd(command);
+			else if (command[0] != NULL)
 				perror("./shell");
-				return (1);
-			}
-		}
-		else
-		{
-			wait(NULL);
 		}
 	}
 	free(line);
